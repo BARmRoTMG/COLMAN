b1ec859582c97db0b786b62ee1ae86e894ce576f
Bar Mashiach <barbar903@walla.com>
2025-05-26 12:25:17 +0300

    may 26 tirgul

--- May26/Main.c
+++ May26/Main.c
@@ -5,7 +5,12 @@ int main()
 	int arr[SIZE] = { -10, 5, -3, 7, 0, -8, 12, -1, 4, -6, 9, -2, 15, -4, 3, -7, 6, -9, 11, -5 };
 	int min = 5;
 	int max = 20;
+	int smaller, bigger;
 
+	check(arr, SIZE, min, max, &smaller, &bigger);
 
-	check(arr, SIZE, );
+	printf("Numbers smaller than %d: %d\n", min, smaller);
+	printf("Numbers greater than %d: %d\n", max, bigger);
+
+	return 0;
 }
--- May26/Util.c
+++ May26/Util.c
@@ -6,8 +6,14 @@ void check(int* numbers, int size, int min, int max, int* p_smaller, int* p_bigg
 	*p_smaller = 0;
 	*p_bigger = 0;
 
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < size; i++)
 	{
+		if (*temp < min)
+			(* p_smaller)++;
 
+		if (*temp > max)
+			(* p_bigger)++;
+
+		temp++;
 	}
 }
